06f34ec4c60035d2322acc670abeb83e10c4e5a8
flavio silva <flavio.devjs@gmail.com>
2021-12-27 06:30:45 -0300

    create repository to database connection

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,30 @@
+import { Category } from "../model/category";
+
+//DTO - Data transfer object
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+
+class CategorieRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category(); 
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date()
+    });
+
+    this.categories.push(category); 
+  }
+}
+
+export { CategorieRepository };
