58bdf1bdcecb9778bde90074f0cfef4fe39d3a48
Flavio Silva <flaviosilva@acalhomecenter.com.br>
2021-12-27 14:39:09 -0300

    create route list and use method findByname to valid category name

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {CategorieRepository} from "../repositories/CategoriesRepository";
+import { CategorieRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategorieRepository();
@@ -7,9 +7,21 @@ const categoriesRepository = new CategorieRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category Already Exists" });
+  }
+
   categoriesRepository.create({ name, description });
 
-  return response.status(201).send()
+  return response.status(201).send();
 });
 
-export { categoriesRoutes } 
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
+});
+
+export { categoriesRoutes };
