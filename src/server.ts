78d22606095b9f8c933e0240efc7a61ec1de98d3
Flavio Silva <flaviosilva@acalhomecenter.com.br>
2021-12-27 14:40:03 -0300

    organization of code

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import express from 'express';
-import {categoriesRoutes} from './routes/categories.routes';
+import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
-const app = express(); 
+const app = express();
 
 app.use(express.json());
 
@@ -11,4 +11,4 @@ app.get("/", (request, response) => {
   return response.json({ message: "Hello World!" });
 });
 
-app.listen(3333, () => console.log("server is running | [port: 3333]..."));
+app.listen(3333, () => console.log("[server is running] | [port: 3333]"));
