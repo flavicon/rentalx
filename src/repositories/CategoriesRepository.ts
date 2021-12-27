b88d011a2d8245a80ad9bcc71672e8c68777f681
Flavio Silva <flaviosilva@acalhomecenter.com.br>
2021-12-27 14:36:06 -0300

    create method findByName

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -14,15 +14,24 @@ class CategorieRepository {
   }
 
   create({ name, description }: ICreateCategoryDTO): void {
-    const category = new Category(); 
+    const category = new Category();
 
     Object.assign(category, {
       name,
       description,
-      created_at: new Date()
+      created_at: new Date(),
     });
 
-    this.categories.push(category); 
+    this.categories.push(category);
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
   }
 }
 
