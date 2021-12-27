29aba174bacc020b217f068c6bae987cfbf26e46
flavio silva <flavio.devjs@gmail.com>
2021-12-27 06:32:04 -0300

    install uuid and modify server.js

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import {categoriesRoutes} from './routes/categories.routes';
 
-const app = express();
+const app = express(); 
+
+app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
 
 app.get("/", (request, response) => {
   return response.json({ message: "Hello World!" });
