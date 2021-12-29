09b95e42033c2e185f66e82ed1ea782b572d10e3
flavio silva <flavio.devjs@gmail.com>
2021-12-29 07:12:40 -0300

    simple updates

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use("/categories", categoriesRoutes);
 
 app.get("/", (request, response) => {
-  return response.json({ message: "Hello World!" });
+  return response.json({ message: "Hello World!!" });
 });
 
 app.listen(3333, () => console.log("[server is running] | [port: 3333]"));
---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,22 @@
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+
+interface IResquest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryService {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  execute({ name, description }: IResquest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error("Category already exists!");
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService };
