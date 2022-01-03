5db07307ac9229d0d63e023d6c034262fa5c9987
flavio silva <flavio.devjs@gmail.com>
2022-01-03 07:13:24 -0300

    Route to specifications services

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+
+import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
+
+const specificationRoutes = Router();
+
+const specificationsRepository = new SpecificationsRepository();
+
+specificationRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+  const createSpecificationService = new CreateSpecificationService(
+    specificationsRepository
+  );
+
+  createSpecificationService.execute({ name, description });
+
+  return response.status(201).send();
+});
+
+export { specificationRoutes };
