f9fa5332477c85ca41886cd3a29445e16851eb43
flavio silva <flavio.devjs@gmail.com>
2022-02-08 05:21:51 -0300

    new updates

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import swaggerUi from "swagger-ui-express";
 import swaggerFile from "./swagger.json";
 import { router } from "./routes";
 
+import "./database";
+
 const app = express();
 
 app.use(express.json());
@@ -11,4 +13,4 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.listen(3333, () => console.log("[server is running] | [port: 3333]"));
+app.listen(3333, () => console.log("[server is running] [port: 3333]"));
